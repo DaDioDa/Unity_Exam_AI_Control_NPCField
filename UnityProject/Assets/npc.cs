10101689a68cfefec5c348621615b07757fbfd61
DaDioDa <56485296+DaDioDa@users.noreply.github.com>
2019-11-07 11:03:04 +0800

    123

---
+++ UnityProject/Assets/npc.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npc : MonoBehaviour {
+    [Header("1234")]
+    [Multiline(4)]
+    public string Say;
+    public float Say_Speed;
+
+    [Header("45656")]
+    public bool Complete;
+    public int Barery_Finish;
+    public int Batery;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ UnityProject/Assets/wads_jump.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wads_jump : MonoBehaviour {
+    [Header("速度")]
+    [Range(0,5)]
+    public float speed;
+    [Header("跳")]
+    [Range(50, 500)]
+    public float jump;
+    [SerializeField]
+    [Header("0001")]
+    private bool ground; 
+    Vector3 gofast = Vector3.zero;
+    Rigidbody mysexybody;
+
+    // Use this for initialization
+    void Start () {
+        mysexybody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+
+    
+    private void FixedUpdate()
+    {
+        movement();
+        marioJump();
+    }
+
+
+    /// <summary>
+    /// WASD移動
+    /// </summary>
+    void movement()
+    {
+        transform.position += new Vector3(Input.GetAxis("Horizontal")*speed, 0.0f, Input.GetAxis("Vertical")*speed);
+    }
+
+
+    /// <summary>
+    /// Space跳
+    /// </summary>
+    void marioJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)&&ground == true)
+        {
+            mysexybody.AddForce(transform.up*jump);
+            ground = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ground = true;
+    }
+}
